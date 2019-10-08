e1e0ff6a91a96feb205df9aec6a568bbdf52f009
PaulLockett <plockett.pl@gmail.com>
2019-10-08 11:56:11 -0400

    Updating flutter dependances

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\plock\Downloads\flutter_windows_v1.5.4-hotfix.2-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\plock\Documents\flutter_hw\snack_app_repo\upgraded-pancake"
+export "FLUTTER_APPLICATION_PATH=C:\Users\plock\Documents\flutter_hw\snack_app_repo\local_snack_selling_app"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
